436932696c0f93339fdec8118fa6fc76f194d4d8
frederikagger <frederikagger@hotmail.com>
2020-11-04 21:20:39 +0100

    small changes

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.todoapp;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,10 +14,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    private ArrayList<String> todos = new ArrayList<>();
+    private ArrayList<Todo> todos = new ArrayList<>();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private ArrayAdapter<String> arrayAdapter;
+    private ArrayAdapter<Todo> arrayAdapter;
+    private ListView mylistview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,32 +25,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setArrayToView();
         listener();
+        attachOnClick();
     }
 
-    private void setArrayToView() {
-        ListView mylistview = findViewById(R.id.listview);
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todos);
-        mylistview.setAdapter(arrayAdapter);
-    }
 
+    public void setArrayToView() {
+        this.mylistview = findViewById(R.id.listview);
+        this.arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todos);
+        this.mylistview.setAdapter(arrayAdapter);
+    }
 
     public void listener(){
-        db.collection("todos").addSnapshotListener((values, error) -> {
+        this.db.collection("todos").addSnapshotListener((values, error) -> {
+            this.todos.clear();
             for(DocumentSnapshot snap : values.getDocuments()){
-                String text1= snap.get("text").toString();
-
-                //Todo todo = new Todo(snap.get("text").toString(), snap.getId());
-                todos.add(text1);
-                arrayAdapter.notifyDataSetChanged();
+                String title = snap.getId();
+                String text = snap.get("text").toString();
+                this.todos.add(new Todo(title, text));
+                this.arrayAdapter.notifyDataSetChanged();
             }
         });
     }
 
+    public void attachOnClick(){
+        this.mylistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                changeActivity(position);
+            }
+        });
+    }
 
-
-    public void changeActivity(AdapterView<?> parent, View view, int position, long id){
+    public void changeActivity(int position){
         Intent intent = new Intent(this, Detailview.class);
-        //intent.putExtra("text", this.notes[position].getText());
+        intent.putExtra("title", this.todos.get(position).getTitle());
+        intent.putExtra("text", this.todos.get(position).getText());
         startActivity(intent);
     }
 }
